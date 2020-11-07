6f614e898b78a786a89abae48c9e4b639a54c46e
gykudo <tukimtuan@gmail.com>
2020-11-07 15:42:48 +0700

    Create product_detail_page_locator.py

---
+++ Locators/product_detail_page_locator.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.common.by import By
+
+
+class ProductDetailPageLocator(object):
+  PRODUCT_NAME_LABEL = (By.XPATH, '')
+  PRODUCT_DESC_LABEL = ()
+  PRODUCT_PRICE_LABEL = ()
+  PRODUCT_ADD_BUTTON = ()
+  PRODUCT_REMOVE_BUTTON = ()
